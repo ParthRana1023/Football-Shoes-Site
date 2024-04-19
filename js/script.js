6fb61d82ee0bc2b93f8de08aec749e8b66eec994
Parth Rana <parth.rana1100@gmail.com>
2024-04-19 15:52:29 +0530

    slight update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 function showSidebar() {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = "flex";
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
 
 function hideSidebar() {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = "none";
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
 }
